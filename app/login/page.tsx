b17946bef3095298d4dd4eac3cc153df3fd8ccc8
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-12-04 10:52:02 -0700

    redirect WIP

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { auth } from '@/auth'
 import LoginForm from '@/components/login-form'
 import { Session } from '@/lib/types'
 import { redirect } from 'next/navigation'
+import { allowedEmails } from '@/lib/allowed-emails'
 
 export default async function LoginPage() {
   const session = (await auth()) as Session
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -19,7 +19,8 @@ export default function LoginForm() {
         toast.error(getMessageFromCode(result.resultCode))
       } else {
         toast.success(getMessageFromCode(result.resultCode))
-        router.refresh()
+        router.push('/redirector')
+        // router.refresh()
       }
     }
   }, [result, router])
