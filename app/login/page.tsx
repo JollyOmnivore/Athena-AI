a00d94b9dcf7a5e98f598a5db15d1034d7903f88
Patrick Rakowicz <patrick.rakowicz@gmail.com>
2024-12-09 10:10:18 -0700

    new Login page w/ Redirect working (#5)
    * redirect WIP
    
    * working redirect, header doesnt update
    
    * Western Components Added
    
    ---------
    
    Co-authored-by: Zay DeHerrera <zay.deherrera@western.edu>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,13 @@
 import { auth } from '@/auth'
 import LoginForm from '@/components/login-form'
 import { Session } from '@/lib/types'
-import { redirect } from 'next/navigation'
 
 export default async function LoginPage() {
   const session = (await auth()) as Session
 
   if (session) {
-    redirect('/')
+    return null
+    // redirect('/')
   }
 
   return (
